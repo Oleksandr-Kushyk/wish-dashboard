be991446508c3fb2c3d314c92bcaaf0df0da959e
Oleksandr-Kushyk <143532611+Oleksandr-Kushyk@users.noreply.github.com>
2023-12-27 20:35:26 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,46 @@
+const user = {
+  name: "Олександр",
+  surname: "Кушик",
+  userPhoto: "IMAGE 2023-09-19 18:56:41.jpg",
+  qrQode: "qr-code.svg",
+  dreams: [
+    { src: "Best-Programming-Languages-to-Start-Learning-Today.jpg.avif", alt: "Вивчити мови програмування" },
+    { src: "ABT-Audi-RS7-Legacy-Edition-5s-1024x576.jpg", alt: "Купити авто моєї мрії" },
+    { src: "rekonst-stroitelstvo-domov-3-1.jpg", alt: "Побудувати будинок" },
+  ],
+};
+
+// arr це середня місячна платня по рокам
+const arr = [
+  500,
+  600,
+  800,
+  1100,
+  1500,
+  1800,
+  2200,
+  2500,
+  2700,
+  3600,
+  4800,
+  6700,
+];
+
+const necessaryExpenses = 800;
+
+const todo = [
+  { month: "Червень", skill: "JavaScript" },
+  { month: "Липень", skill: "Java" },
+  { month: "Серпень", skill: "Python" },
+  { month: "Вересень", skill: "Самостійність" },
+  { month: "Жовтень", skill: "Гнучкість" },
+  { month: "Листопад", skill: "Емоційний інтелект" },
+  { month: "Грудень", skill: "Креативність" },
+  { month: "Січень", skill: "Лідерство" },
+  { month: "Лютий", skill: "Нетворкінг" },
+  { month: "Березень", skill: "Вміння працювати у різних програмах" },
+  { month: "Квітень", skill: "Управління фінансами" },
+  { month: "Травень", skill: "Публічні виступи" },
+];
+
+export { user, arr, necessaryExpenses, todo };
